a3dc45ca4eb1689921623ab3e7429286d0049f22
Psrweblearner <psrweblearner1471996@gmail.com>
2019-01-20 21:28:32 +0530

    re-changed home to homepage

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.homepage, name='home'),
     path('countss', views.count, name='count')
    
 ]
